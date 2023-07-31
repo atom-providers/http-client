2b5ae365b6e57200b81c21163ee7f52c0835e78d
yanghao05 <yanghao05@qianxin.com>
2023-07-31 17:21:33 +0800

    first commit

---
+++ config.go
@@ -0,0 +1,35 @@
+package httpClient
+
+import (
+	"github.com/rogeecn/atom-addons/providers/http"
+	"github.com/rogeecn/atom/container"
+	"github.com/rogeecn/atom/utils/opt"
+)
+
+const DefaultPrefix = "HttpClient"
+
+func DefaultProvider() container.ProviderContainer {
+	return container.ProviderContainer{
+		Provider: Provide,
+		Options: []opt.Option{
+			opt.Prefix(http.DefaultPrefix),
+		},
+	}
+}
+
+type Config struct {
+	DevMode            bool
+	CookieJarFile      string
+	RootCa             []string
+	UserAgent          string
+	InsecureSkipVerify bool
+	CommonHeaders      map[string]string
+	Timeout            uint
+	AuthBasic          struct {
+		Username string
+		Password string
+	}
+	AuthBearerToken string
+	ProxyURL        string
+	RedirectPolicy  []string // "Max:10;No;SameDomain;SameHost;AllowedHost:x,x,x,x,x,AllowedDomain:x,x,x,x,x"
+}
